dd1fb602cc90df12439b4848da7a305dda895d1a
Vijaylaxmi Edke <vijaedke@in.ibm.com>
2020-10-27 23:18:59 +0530

    GCC-0004 - Updated addUser bash script with validations and mongo commands

--- cmd/addUser.sh
+++ cmd/addUser.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-echo "Add User"
+DBNAME = $1
+DBUSER = $2
+DBPWD  = $3
+DBURL  = $4
+COLLECTION_NAME = "users"
+
+echo "Add Default admin user to the system"
+
+echo "=============================================================================="
+echo "====================Checking Mongo Configurations============================="
+echo "=============================================================================="
+
+echo `mongo --version`
+echo `mongo --eval "printjson(db.serverStatus()) --quiet" `
+
+createDBUser = `mongo --eval "printjson(db.createUser({user: $DBUSER, pwd: $DBPWD, roles: []}))"`
+if [ $createDBUser -ne 0 ]; then
+    echo "$DBUSER already exists"
+    exit(0)
+else
+    echo "$DBUSER added"
+    echo "=============================================================================="
+    echo "=============================Adding Mongo User================================"
+    echo "=============================================================================="
+
+    echo `mongo --eval $DBURL/$DBNAME 'var document = { username : "admin", role : ["system_admin"] }; db.${COLLECTION_NAME}.insert(document);'`
+fi
